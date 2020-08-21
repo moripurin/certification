ff3060d8e3bd64fd85cf13350b43e6d711568bd2
moripurin <rnori.pudding@gmail.com>
2020-08-22 01:32:28 +0900

    feat: vue-auth module

---
+++ src/components/Login.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
